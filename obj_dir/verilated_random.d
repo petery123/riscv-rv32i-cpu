9c0b5283076f3368a4db7bd0e1960a4aa3b4c75d
Peter Yakubu <139976625+petery123@users.noreply.github.com>
2026-09-13 02:29:22 -0400

    Clean generated program files

--- obj_dir/verilated_random.d
+++
@@ -1,7 +0,0 @@
-verilated_random.o: /home/yakub/verilator/include/verilated_random.cpp \
- /home/yakub/verilator/include/verilated_random.h \
- /home/yakub/verilator/include/verilated.h \
- /home/yakub/verilator/include/verilated_config.h \
- /home/yakub/verilator/include/verilatedos.h \
- /home/yakub/verilator/include/verilated_types.h \
- /home/yakub/verilator/include/verilated_funcs.h
